c698d1e8b3744f7a71a7791d848a5f5207444fee
Alsakr922 <mo7amedk7aled922@gmail.com>
2025-04-27 18:09:41 +0300

    first commit

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,7 +9,7 @@ const Project = ({ img, title, description , reverse,href,rounded }) => {
       }`}>
       <img
         src={img}
-        className={`sm:w-1/2  ${
+        className={`w-1/2  ${
           rounded
             ? rounded === "l"
               ? "rounded-l-full"
